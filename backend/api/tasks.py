b7831b0ce871880aab448db9ea7b66c20b03070d
Ahmed-Tedj-Eddine-Ramoul <oa_ramoul@esi.dz>
2025-10-03 18:16:07 +0100

    fix: added permission check to the function

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 from bson import ObjectId
 from database.mongo_config import connection
 from typing import Optional, Dict, List, Any
+from .users import haspermission, getorcreateuser
 
 db = connection()
 tasks = db["tasks"]
@@ -9,7 +10,8 @@ class TaskModel:
 
 #The function is used to initialize a register of fields   
 
-    def __init__(self, title: str, description: Optional[str]=None, 
+    def __init__(self, title: str,  task_creator: str, 
+                  description: Optional[str]=None,
                   assignees: Optional[List[str]]=None,
                   deadline: Optional[datetime]=None,
                   taskid: Optional[str]=None,
@@ -18,6 +20,7 @@ class TaskModel:
                   date_of_creation: Optional[datetime]=None):
         self._id= ObjectId(taskid) if taskid else ObjectId()
         self.title= title
+        self.createdby= task_creator
         self.description = description
         self.assignees = assignees or []
         self.status = status
@@ -31,6 +34,7 @@ class TaskModel:
         return {
             "_id": self._id,
             "title": self.title,
+            "createdby": self.createdby,
             "description": self.description,
             "assignees": self.assignees,
             "status": self.status,
@@ -43,10 +47,11 @@ class TaskModel:
     def from_dict(cls, data: Dict[str, Any]) -> "TaskModel": #This function is used for converting an object from JSON, BSON format to python object
       return cls(       
         title= data.get("title"),
+        created_by= data.get("createdby"),
         description= data.get("description"),
         assignees= data.get("assignees", []),
         taskid= str(data.get("_id")) if data.get("_id") else None,
-        status= data["status","Pending" ],    
+        status= data.get("status","Pending" ),    
         priority= data.get("priority", "Normal"),
         deadline= data.get("deadline"),
         date_of_creation= data.get("date_of_creation", datetime.now(timezone.utc))
@@ -58,17 +63,23 @@ class TaskModel:
     
     """ 
 
-def create_task(userid, title, description, priority, deadline ):
-         task = TaskModel(
-              title= title,
-              description= description,
-              assignees= [userid],
-              deadline= deadline,
-              priority= priority
-         )
-         tasks.insert_one(task.todict())
-         return task.todict()
-    
+def create_task(ifalloweduser: dict , userid, title, description, priority, deadline ):
+         token = ifalloweduser.get("_id")
+         if token:
+             task = TaskModel(
+                 title= title,
+                 task_creator=ifalloweduser.get("username"),
+                 description= description,
+                 assignees= [userid],
+                 deadline= deadline,
+                 priority= priority
+            )
+             tasks.insert_one(task.todict())
+             return task.todict()
+         else:
+              print("permission isn't granted!")
+              return None
+
   
 """
 
@@ -76,9 +87,13 @@ def create_task(userid, title, description, priority, deadline ):
     
 """
 
-def get_task(taskid):
-       return tasks.find_one(ObjectId(taskid))
-    
+def get_task(taskid, ifalloweduser: dict ):
+       task= tasks.find_one({"_id": taskid})
+       token = haspermission("get_task", ifalloweduser, task)
+       if token:
+            return task
+       else:
+            return None
 """
 
 This function will list all the tasks availabe in the database
@@ -93,7 +108,7 @@ types of filters: {} : lists all the tasks that exist in the db
 def list_tasks(filters=None):
        if filters is None:
           filters = {} 
-       return list(tasks.find_one(filters))  
+       return list(tasks.find(filters))  
 
 """
 
@@ -102,12 +117,17 @@ def list_tasks(filters=None):
  
 """
 
-def assign_task(taskid, assigneeid):
-    tasks.update_one(
+def assign_task(taskid, assigneeid, ifalloweduser: dict):
+    task = tasks.find_one({"_id": taskid})
+    token = haspermission("assign_task", ifalloweduser, task)
+    if token:
+        tasks.update_one(
              {"_id" :ObjectId(taskid)},
              {"$addToSet": {"assignees": assigneeid}} 
         )
-    return get_task(taskid)
+        return tasks.find_one(ObjectId(taskid))
+    else:
+         return None
 """
    
      This function will mark a task as done by  and adds their id
@@ -116,13 +136,17 @@ def assign_task(taskid, assigneeid):
 
 """
   
-def mark_task_done (taskid, donebyid):
-       tasks.update_one(
-          {"_id" :ObjectId(taskid)},
-          {"$set": {"status": "Done"}, "$pull": {"assignees": donebyid}}
-        )
-       return get_task(taskid)
-    
+def mark_task_done (taskid, ifalloweduser: dict, donebyid=None): 
+       task = tasks.find_one(ObjectId(taskid))     
+       token = haspermission("mark_task_done", ifalloweduser, task)
+       if token: 
+        tasks.update_one(
+              {"_id" :ObjectId(taskid)},
+              {"$set": {"status": "Done"}, "$pull": {"assignees": donebyid}}
+            )
+        return tasks.find_one(ObjectId(taskid))
+       else:
+            return None
 """   
 
     This function will mark a task cancelled and register who cancelled it
@@ -131,12 +155,19 @@ def mark_task_done (taskid, donebyid):
 
 """
 
-def cancel_task(taskid, cancelledbyid):
-      tasks.update_one(
-           {"_id": ObjectId(taskid)},
-           {"$set": {"status": "Cancelled", "cancelled by": cancelledbyid, "cancelled at": datetime.now(tz=timezone.utc)}}
-      )
-      return get_task(taskid)
+def cancel_task(taskid, ifalloweduser: dict):
+      task = tasks.find_one(ObjectId(taskid))
+      token = haspermission("cancel_task", ifalloweduser, task)
+      if token:
+        cancelledbyid= ifalloweduser.get("_id")
+        tasks.update_one(
+               {"_id": ObjectId(taskid)},
+               {"$set": {"status": "Cancelled", "cancelled by": cancelledbyid, "cancelled at": datetime.now(tz=timezone.utc)}}
+        )
+        return tasks.find_one(ObjectId(taskid))
+      else:  
+           return None
+        
       
 
    
